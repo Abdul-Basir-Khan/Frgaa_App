38d28bb6742316829374ce9275021148931a8831
Abdul-Basir-Khan <abdulbak750@gmail.com>
2023-12-31 22:43:10 +0500

    first commit

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const mainColor = Color(0xFF2B2D5E);
+  static const mainLight = Color(0xff2F3164);
+  static const primaryWhite = Color(0xFFFFFFFF);
+  static const primaryBlack = Color(0xFF000000);
+  static const iconColor = Color(0xFF1C1E3D);
+  static const bColor = Color(0xFF5A3D77);
+}
